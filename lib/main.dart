25c7b5232dd4f8c5b053cd5ce9e079d28433c447
christiandarvs <christiandarvin4@gmail.com>
2023-02-05 00:25:30 +0800

    add container with an img then, add boxshadow

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,30 @@ class Saturday extends StatelessWidget {
         backgroundColor: Colors.blueGrey,
         title: const Text('Saturday - Study'),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+              width: 400,
+              decoration: BoxDecoration(
+                  image: const DecorationImage(
+                      image: NetworkImage(
+                          'https://images.pexels.com/photos/13739093/pexels-photo-13739093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                      fit: BoxFit.cover),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(10, 10),
+                        blurRadius: 5.0,
+                        spreadRadius: 2.0)
+                  ],
+                  borderRadius: BorderRadius.circular(20)),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
