f399ca28b4609aed8f97e795381f42f2c575a3e8
christiandarvs <christiandarvin4@gmail.com>
2023-02-08 16:48:10 +0800

    add wednesday class

--- lib/main.dart
+++ lib/main.dart
@@ -611,3 +611,17 @@ class Tuesday extends StatelessWidget {
     );
   }
 }
+
+class Wednesday extends StatelessWidget {
+  const Wednesday({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Column(),
+      )),
+    );
+  }
+}
